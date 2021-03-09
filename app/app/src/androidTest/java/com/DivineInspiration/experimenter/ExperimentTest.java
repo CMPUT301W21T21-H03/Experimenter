5d44b7a1d2ec1e5dcafdae1926fdf622af173950
Zeyu-Li <zeyu.li23@gmail.com>
2021-03-08 22:03:33 -0600

    started javaDoc

---
+++ app/app/src/androidTest/java/com/DivineInspiration/experimenter/ExperimentTest.java
@@ -0,0 +1,10 @@
+package com.DivineInspiration.experimenter;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExperimentTest {
+
+    @org.junit.jupiter.api.Test
+    void getExperimentName() {
+    }
+}
