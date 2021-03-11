973731b8fa7f1c48eeebab3c1353742fbabe2b83
Zeyu-Li <zeyu.li23@gmail.com>
2021-03-10 18:51:03 -0600

    moved test to tests

--- app/app/src/test/java/com/DivineInspiration/experimenter/ContactPersonInfoTest.java
+++ app/app/src/test/java/com/DivineInspiration/experimenter/ContactPersonInfoTest.java
@@ -1,8 +1,15 @@
 package com.DivineInspiration.experimenter;
 
+import android.util.Log;
+
 import com.DivineInspiration.experimenter.Model.ContactPersonInfo;
+import com.google.firebase.installations.FirebaseInstallations;
 
 import org.junit.Test;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
+
 import static org.junit.Assert.*;
 
 public class ContactPersonInfoTest {
@@ -54,5 +61,33 @@ public class ContactPersonInfoTest {
 
         mock.setCityName("Edmonton");
         assertEquals("Edmonton", mock.getCityName());
+    }
+
+    public static class TestInstallation {
+        public static long testInstallation(){
+            final AtomicLong output = new AtomicLong(0);
+            final AtomicBoolean done = new AtomicBoolean(false);
+            FirebaseInstallations.getInstance().getId().addOnCompleteListener(task -> {
+                Log.d("stuff","entering get Id");
+                if(task.isSuccessful()){
+                    Log.d("stuff",task.getResult() + "||||||||||||||| Task is successful");
+                    output.set(333);
+                }
+                else{
+                    Log.d("stuff","task failed");
+                }
+                Log.d("stuff","existing getId");
+                done.set(true);
+            });
+            Log.d("stuff", "done?    "+ String.valueOf(done.get()));
+    //        while(!done.get()){
+    //            try {
+    //                Thread.sleep(25L);
+    //            } catch (InterruptedException e) {
+    //                e.printStackTrace();
+    //            }
+    //        };
+            return output.get();
+        }
     }
 }
