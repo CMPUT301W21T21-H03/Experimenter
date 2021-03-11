7ca009d9be17ff8db317dc20116971d19bca8a3c
Aniket Mishra <ianiket23@gmail.com>
2021-03-11 16:56:05 -0700

    changes

--- app/app/src/main/java/com/DivineInspiration/experimenter/Activity/MainActivity.java
+++ app/app/src/main/java/com/DivineInspiration/experimenter/Activity/MainActivity.java
@@ -15,9 +15,20 @@ public class MainActivity extends AppCompatActivity implements LocalUserManager.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+<<<<<<< Updated upstream
         setContentView(R.layout.activity_main);
 
 
+=======
+        setContentView(R.layout.user_profile);
+//        BottomNavigationView navigationView = findViewById(R.id.bottom_navigation);
+//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+//          R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+//                .build();
+//        NavController navController = Navigation.findNavController(this,R.id.mainFrag);
+//        NavigationUI.setupActionBarWithNavController(this,navController,appBarConfiguration);
+//        NavigationUI.setupWithNavController(navigationView,navController);
+>>>>>>> Stashed changes
 
 
         manager.setContext(this);
