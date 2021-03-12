70c43423c262c43810fe64bd6e6e0008bb649937
Goldentoaste <a2375658@gmail.com>
2021-03-12 03:02:11 -0800

    bruh

--- app/app/src/main/java/com/DivineInspiration/experimenter/Activity/ui/home/HomeFragment.java
+++ app/app/src/main/java/com/DivineInspiration/experimenter/Activity/ui/home/HomeFragment.java
@@ -2,9 +2,14 @@ package com.DivineInspiration.experimenter.Activity.ui.home;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -14,11 +19,34 @@ import com.DivineInspiration.experimenter.R;
 
 public class HomeFragment extends Fragment {
 
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
+    public HomeFragment(){
+        super(R.layout.fragment_home);
+    }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-        return root;
+    }
+
+    /*
+    https://stackoverflow.com/questions/31231609/creating-a-button-in-android-toolbar
+     */
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.profile_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.editProfileButt:
+                Toast.makeText(getContext(), "stuff", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 }
