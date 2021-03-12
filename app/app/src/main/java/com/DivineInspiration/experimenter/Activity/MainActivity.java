88430455373be020803008f478669dea2638f65c
Aniket Mishra <ianiket23@gmail.com>
2021-03-11 18:19:39 -0700

    Temporary Skeleton Sort of

--- app/app/src/main/java/com/DivineInspiration/experimenter/Activity/MainActivity.java
+++ app/app/src/main/java/com/DivineInspiration/experimenter/Activity/MainActivity.java
@@ -21,14 +21,17 @@ public class MainActivity extends AppCompatActivity implements LocalUserManager.
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
         manager.setContext(this);
         manager.setReadyCallback(this);
 
-
-
     }
-
 
     @Override
     public void onUserReady() {
