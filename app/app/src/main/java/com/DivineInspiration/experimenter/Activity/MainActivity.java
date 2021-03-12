fe8225715c646ae9c2dccd8a77c813637b9d26a0
Goldentoaste <a2375658@gmail.com>
2021-03-11 17:31:30 -0800

    temp nav stuff

--- app/app/src/main/java/com/DivineInspiration/experimenter/Activity/MainActivity.java
+++ app/app/src/main/java/com/DivineInspiration/experimenter/Activity/MainActivity.java
@@ -1,12 +1,19 @@
 package com.DivineInspiration.experimenter.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.util.Log;
 
 import com.DivineInspiration.experimenter.Controller.LocalUserManager;
 import com.DivineInspiration.experimenter.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity implements LocalUserManager.UserReadyCallback {
@@ -17,8 +24,24 @@ public class MainActivity extends AppCompatActivity implements LocalUserManager.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        BottomNavigationView nav = findViewById(R.id.bottom_navigation);
+
+        /*
+        https://www.section.io/engineering-education/bottom-navigation-bar-in-android/
+         */
 
 
+        //Initialize NavController.
+        NavHostFragment navHostFragment =
+                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host);
+        NavController navController = navHostFragment.getNavController();
+        //Pass the ID's of Different destinations
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+               navController.getGraph())
+                .build();
+
+    //    NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(nav, navController);
         //setup local user
         manager.setContext(this);
         manager.setReadyCallback(this);
