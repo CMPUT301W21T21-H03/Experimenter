0df680cca301eae2d2abf46095fb88b90edfad9b
Aniket Mishra <ianiket23@gmail.com>
2021-03-13 11:49:12 -0700

    Update MainActivity.java

--- app/app/src/main/java/com/DivineInspiration/experimenter/Activity/MainActivity.java
+++ app/app/src/main/java/com/DivineInspiration/experimenter/Activity/MainActivity.java
@@ -1,25 +1,16 @@
 package com.DivineInspiration.experimenter.Activity;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
-import android.util.Log;
 
 import com.DivineInspiration.experimenter.Controller.LocalUserManager;
 import com.DivineInspiration.experimenter.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity implements LocalUserManager.UserReadyCallback {
 
@@ -33,7 +24,7 @@ public class MainActivity extends AppCompatActivity implements LocalUserManager.
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_scan, R.id.navigation_explore)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
       //  NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
