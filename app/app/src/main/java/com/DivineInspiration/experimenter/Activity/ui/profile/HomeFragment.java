4834cea77b4148f9f4f314c5577329ea4c72458b
Aniket Mishra <ianiket23@gmail.com>
2021-03-13 11:50:18 -0700

    Color scheme

--- app/app/src/main/java/com/DivineInspiration/experimenter/Activity/ui/profile/HomeFragment.java
+++ app/app/src/main/java/com/DivineInspiration/experimenter/Activity/ui/profile/HomeFragment.java
@@ -1,20 +1,16 @@
-package com.DivineInspiration.experimenter.Activity.ui.home;
+package com.DivineInspiration.experimenter.Activity.ui.profile;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
+
 import com.DivineInspiration.experimenter.R;
 
 public class HomeFragment extends Fragment {
