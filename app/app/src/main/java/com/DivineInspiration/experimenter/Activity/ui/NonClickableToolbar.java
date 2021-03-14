7876ae8ee87de964d0c07ee3a10843c246f32623
Goldentoaste <a2375658@gmail.com>
2021-03-14 00:59:01 -0800

    fixed again

--- app/app/src/main/java/com/DivineInspiration/experimenter/Activity/ui/NonClickableToolbar.java
+++ app/app/src/main/java/com/DivineInspiration/experimenter/Activity/ui/NonClickableToolbar.java
@@ -8,6 +8,10 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
 
+/*
+no clikcing: https://stackoverflow.com/questions/30425786/how-to-click-views-behind-a-toolbar/62691616#62691616
+missing contructers: https://stackoverflow.com/questions/9054894/custom-surfaceview-causing-nosuchmethodexception
+ */
 public class NonClickableToolbar extends Toolbar {
     public NonClickableToolbar(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
