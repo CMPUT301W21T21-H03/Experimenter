43c3e05b9e2fc6a1670ec99184ae3293786e2c19
Goldentoaste <a2375658@gmail.com>
2021-03-18 02:00:07 -0700

    experiment manager done

--- app/app/src/main/java/com/DivineInspiration/experimenter/Controller/ExperimentManager.java
+++ app/app/src/main/java/com/DivineInspiration/experimenter/Controller/ExperimentManager.java
@@ -6,12 +6,15 @@ import androidx.annotation.NonNull;
 
 import com.DivineInspiration.experimenter.Model.Experiment;
 import com.DivineInspiration.experimenter.Model.IdGen;
+import com.DivineInspiration.experimenter.Model.UserContactInfo;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
@@ -27,11 +30,8 @@ public class ExperimentManager extends ArrayList<Experiment> {
     final String TAG = "ExperimentManager";
 
     public interface ExperimentReadyCallback{
-<<<<<<< Updated upstream
-         void onExperimentReady();
-=======
          void onExperimentsReady(List<Experiment> experiments);
->>>>>>> Stashed changes
+
     }
 
 
@@ -94,10 +94,7 @@ public class ExperimentManager extends ArrayList<Experiment> {
             }
         });
 
-<<<<<<< Updated upstream
-    public ArrayList<Experiment> getList(){
-        return experiments;
-    }
+
 
     private ExperimentManager(){
         experiments = new ArrayList<>();
@@ -109,7 +106,7 @@ public class ExperimentManager extends ArrayList<Experiment> {
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if(task.isSuccessful()){
 
-=======
+
     }
 
     public void querySearch(String keywords, ExperimentReadyCallback callback){
@@ -132,164 +129,73 @@ public class ExperimentManager extends ArrayList<Experiment> {
                 }
                 else{
                     Log.d("stuff", "query user subscriptions failed!");
->>>>>>> Stashed changes
                 }
             }
         });
     }
 
 
-    public ArrayList<Experiment> getExperiments1(){
-
-        Task<QuerySnapshot> queryTask = db.collection("Experiments").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-<<<<<<< Updated upstream
-                if (task.isSuccessful()) {
-                    Log.d(TAG, "Success retrieving documents");
-                } else {
-                    Log.d(TAG, "Error retrieving documents", task.getException());
-=======
-                if(task.isSuccessful()){
-                    if(callback!=null){
-                        List<Experiment> output = new ArrayList<>();
-                        for (QueryDocumentSnapshot snapshot: task.getResult()){
-                            output.add(expFromSnapshot(snapshot));
-                        }
-                        callback.onExperimentsReady(output);
-                    }
-                }
-                else{
-                    Log.d("stuff", "query user experiments failed!");
->>>>>>> Stashed changes
-                }
-            }
-        });
-
-        while(!queryTask.isComplete()) { }
-
-        QuerySnapshot query = queryTask.getResult();
-        List<DocumentSnapshot> queryData = query.getDocuments();
-
-        experiments.clear();
-        for (DocumentSnapshot doc : queryData) {
-
-            String name = doc.get("Name").toString();
-            String oID = doc.get("OwnerID").toString();
-            String description = doc.get("Description").toString();
-            String eID = doc.get("ExperimentID").toString();
-
-            Task<DocumentSnapshot> userQueryTask = db.collection("Users").document(oID).get();
-            while(!userQueryTask.isComplete()) { }
-
-            DocumentSnapshot userQuery = userQueryTask.getResult();
-
-            String username = userQuery.get("UserName").toString();
-            String userDescription = userQuery.get("UserDescription").toString();
-            String address = userQuery.get("Address").toString();
-            String city = userQuery.get("CityName").toString();
-            String email = userQuery.get("Email").toString();
-            String phone = userQuery.get("PhoneNumber").toString();
-
-            UserContactInfo contactInfo = new UserContactInfo(city, email);
-            User owner = new User(username, oID, contactInfo, userDescription);
-
-            Experiment experiment = new Experiment(name, owner, description, eID);
-        }
-
-        return experiments;
-    }
-
-<<<<<<< Updated upstream
-    public static ExperimentManager getInstance(){
-        if(singleton == null){
-            singleton = new ExperimentManager();
-        }
-
-        return singleton;
-    }
-
-    /**
-    * Adds an experiment to the database
-    * @param experiment
-    * @return
-     *      returns true if the experiment is added successfully
-     *      returns false if the experiment is not added successfully
-     */
-    public boolean addExperiment1(Experiment experiment) {
-
-        Log.d(TAG, "addExperiment: Working");
-
-        boolean successful = true;
-        Map<String, Object> doc = new HashMap<>();
-        doc.put("Name", experiment.getExperimentName());
-        doc.put("OwnerID", experiment.getExperimentOwner().getUserId());
-        doc.put("ExperimentID", experiment.getExperimentID());
-        doc.put("Description", experiment.getExperimentDescription());
-        Map<String, Object> Subscribers = new HashMap<>();
-        Subscribers.put("0", experiment.getExperimentOwner().getUserId() );
-
-        Log.d(TAG, "working 2");
-
-        db.collection("Experiments")
-                    .document(experiment.getExperimentID())
-                    .set(doc)
-                    .addOnSuccessListener(new OnSuccessListener<Void>() {
-                        @Override
-                        public void onSuccess(Void aVoid) {
-                            Log.d(TAG, "working 4");
-                        }
-                    });
-
-
-            return true;
-    }
-
-    /**
-     * Checks if an experiment is in the database. Searches by id
-     * @param experiment
-     * @return
-     *      returns true if that experiment exists in the database
-     *      returns false if the experiment does not exist in the database
-     */
-    public boolean hasExperiment1(Experiment experiment) {
-
-        String experimentID = experiment.getExperimentID();
-        Task<DocumentSnapshot> queryTask = db.collection("Experiments").document(experimentID).get();
-
-        while(!queryTask.isComplete()) { }
-
-        return queryTask.isSuccessful();
-    }
-
-    /**
-     * Checks if an experiment is in the database. Searches by id
-     * @param experimentID
-     * @return
-     *      returns true if that experiment exists in the database
-     *      returns false if the experiment does not exist in the database
-     */
-    public boolean hasExperiment1(String experimentID) {
-        Log.d(TAG, "working Has experiment");
-        Task<DocumentSnapshot> queryTask = db.collection("Experiments").document(experimentID).get();
-        Log.d(TAG, "working Has experiment Task");
-        while(!queryTask.isComplete()) { }
-
-        Log.d(TAG, "working Has experiment2");
-        return queryTask.isSuccessful();
-    }
-=======
-    private Experiment expFromSnapshot(QueryDocumentSnapshot snapshot){
-        return new Experiment(
-               snapshot.getId(),
-                snapshot.getString("ExperimentName"),
-                snapshot.getString("OwnerID"),
-                snapshot.getString("ExperimentDescription"),
-                Objects.requireNonNull(snapshot.getLong("TrialType")).intValue(),
-                snapshot.getString("Region"),
-                Objects.requireNonNull(snapshot.getLong("MinimumTrials")).intValue()
-        );
-    }
+//    public ArrayList<Experiment> getExperiments1(){
+//
+//        Task<QuerySnapshot> queryTask = db.collection("Experiments").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//            @Override
+//            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//<<<<<<< Updated upstream
+//                if (task.isSuccessful()) {
+//                    Log.d(TAG, "Success retrieving documents");
+//                } else {
+//                    Log.d(TAG, "Error retrieving documents", task.getException());
+//=======
+//                if(task.isSuccessful()){
+//                    if(callback!=null){
+//                        List<Experiment> output = new ArrayList<>();
+//                        for (QueryDocumentSnapshot snapshot: task.getResult()){
+//                            output.add(expFromSnapshot(snapshot));
+//                        }
+//                        callback.onExperimentsReady(output);
+//                    }
+//                }
+//                else{
+//                    Log.d("stuff", "query user experiments failed!");
+//>>>>>>> Stashed changes
+//                }
+//            }
+//        });
+//
+//        while(!queryTask.isComplete()) { }
+//
+//        QuerySnapshot query = queryTask.getResult();
+//        List<DocumentSnapshot> queryData = query.getDocuments();
+//
+//        experiments.clear();
+//        for (DocumentSnapshot doc : queryData) {
+//
+//            String name = doc.get("Name").toString();
+//            String oID = doc.get("OwnerID").toString();
+//            String description = doc.get("Description").toString();
+//            String eID = doc.get("ExperimentID").toString();
+//
+//            Task<DocumentSnapshot> userQueryTask = db.collection("Users").document(oID).get();
+//            while(!userQueryTask.isComplete()) { }
+//
+//            DocumentSnapshot userQuery = userQueryTask.getResult();
+//
+//            String username = userQuery.get("UserName").toString();
+//            String userDescription = userQuery.get("UserDescription").toString();
+//            String address = userQuery.get("Address").toString();
+//            String city = userQuery.get("CityName").toString();
+//            String email = userQuery.get("Email").toString();
+//            String phone = userQuery.get("PhoneNumber").toString();
+//
+//            UserContactInfo contactInfo = new UserContactInfo(city, email);
+//            User owner = new User(username, oID, contactInfo, userDescription);
+//
+//            Experiment experiment = new Experiment(name, owner, description, eID);
+//        }
+//
+//        return experiments;
+//    }
+//
 
     public static ExperimentManager getInstance(){
         if(singleton == null){
@@ -299,7 +205,97 @@ public class ExperimentManager extends ArrayList<Experiment> {
         return singleton;
     }
 
-
+//    /**
+//    * Adds an experiment to the database
+//    * @param experiment
+//    * @return
+//     *      returns true if the experiment is added successfully
+//     *      returns false if the experiment is not added successfully
+//     */
+//    public boolean addExperiment1(Experiment experiment) {
+//
+//        Log.d(TAG, "addExperiment: Working");
+//
+//        boolean successful = true;
+//        Map<String, Object> doc = new HashMap<>();
+//        doc.put("Name", experiment.getExperimentName());
+//        doc.put("OwnerID", experiment.getExperimentOwner().getUserId());
+//        doc.put("ExperimentID", experiment.getExperimentID());
+//        doc.put("Description", experiment.getExperimentDescription());
+//        Map<String, Object> Subscribers = new HashMap<>();
+//        Subscribers.put("0", experiment.getExperimentOwner().getUserId() );
+//
+//        Log.d(TAG, "working 2");
+//
+//        db.collection("Experiments")
+//                    .document(experiment.getExperimentID())
+//                    .set(doc)
+//                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+//                        @Override
+//                        public void onSuccess(Void aVoid) {
+//                            Log.d(TAG, "working 4");
+//                        }
+//                    });
+//
+//
+//            return true;
+//    }
+//
+//    /**
+//     * Checks if an experiment is in the database. Searches by id
+//     * @param experiment
+//     * @return
+//     *      returns true if that experiment exists in the database
+//     *      returns false if the experiment does not exist in the database
+//     */
+//    public boolean hasExperiment1(Experiment experiment) {
+//
+//        String experimentID = experiment.getExperimentID();
+//        Task<DocumentSnapshot> queryTask = db.collection("Experiments").document(experimentID).get();
+//
+//        while(!queryTask.isComplete()) { }
+//
+//        return queryTask.isSuccessful();
+//    }
+//
+//    /**
+//     * Checks if an experiment is in the database. Searches by id
+//     * @param experimentID
+//     * @return
+//     *      returns true if that experiment exists in the database
+//     *      returns false if the experiment does not exist in the database
+//     */
+//    public boolean hasExperiment1(String experimentID) {
+//        Log.d(TAG, "working Has experiment");
+//        Task<DocumentSnapshot> queryTask = db.collection("Experiments").document(experimentID).get();
+//        Log.d(TAG, "working Has experiment Task");
+//        while(!queryTask.isComplete()) { }
+//
+//        Log.d(TAG, "working Has experiment2");
+//        return queryTask.isSuccessful();
+//    }
+//=======
+//    private Experiment expFromSnapshot(QueryDocumentSnapshot snapshot){
+//        return new Experiment(
+//               snapshot.getId(),
+//                snapshot.getString("ExperimentName"),
+//                snapshot.getString("OwnerID"),
+//                snapshot.getString("ExperimentDescription"),
+//                Objects.requireNonNull(snapshot.getLong("TrialType")).intValue(),
+//                snapshot.getString("Region"),
+//                Objects.requireNonNull(snapshot.getLong("MinimumTrials")).intValue()
+//        );
+//    }
+//
+//    public static ExperimentManager getInstance(){
+//        if(singleton == null){
+//            singleton = new ExperimentManager();
+//        }
+//
+//        return singleton;
+//    }
+//
+//
 
 
 //    public ArrayList<Experiment> getExperiments1(){
