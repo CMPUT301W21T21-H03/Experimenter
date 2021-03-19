b59284ea3b9ea9b2bcf80bfa875a663d17edfbd9
Sheldon Nitz <73610601+primals@users.noreply.github.com>
2021-03-19 02:45:42 -0600

    non NegTrialTest

--- app/app/src/test/java/com/DivineInspiration/experimenter/NonNegativeTrialTest.java
+++ app/app/src/test/java/com/DivineInspiration/experimenter/NonNegativeTrialTest.java
@@ -1,4 +1,56 @@
 package com.DivineInspiration.experimenter;
 
+import com.DivineInspiration.experimenter.Model.Experiment;
+import com.DivineInspiration.experimenter.Model.Trial.BinomialTrial;
+import com.DivineInspiration.experimenter.Model.Trial.MeasurementTrial;
+import com.DivineInspiration.experimenter.Model.Trial.NonNegativeTrial;
+import com.DivineInspiration.experimenter.Model.User;
+import com.DivineInspiration.experimenter.Model.UserContactInfo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class NonNegativeTrialTest {
+
+    private NonNegativeTrial mockNonNegativeTrial() {
+        User user = mockTrialOwner();
+        Experiment experiment = mockExperiment();
+        return new NonNegativeTrial(user, experiment.getExperimentID());
+    }
+
+
+    private Experiment mockExperiment() {
+        Experiment experiment = new Experiment("EXPQQ7FKJB9CVDUE", "Pass or Fail",
+                mockExperimentOwner().getUserId() , "keeps count if you failed or passed",
+                1, "Edmonton", 10, true);
+        return experiment;
+    }
+    private User mockTrialOwner() {
+        User user = new User("Edmonton", "XDC23ABC9K", mockContactInfo(), "test description!!!!");
+        return user;
+    }
+    private User mockExperimentOwner() {
+        User user = new User("Calgary", "XDC23DEF9K", mockContactInfo(), "aoooga");
+        return user;
+    }
+
+    private UserContactInfo mockContactInfo() {
+        return new UserContactInfo("Edmonton", "abcd@gmail.com");
+    }
+
+    @Test
+    public void addCountTest() {
+        NonNegativeTrial trial = mockNonNegativeTrial();
+        assertEquals(0, trial.getCount());
+
+        trial.addCount();
+        assertEquals(1, trial.getCount());
+
+        for (int i = 0; i < 100; i++) {
+            trial.addCount();
+        }
+        assertEquals(101, trial.getCount());
+
+    }
 }
