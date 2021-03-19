bb5250ee7f04201d6a780637fc898073846f5b23
Sheldon Nitz <73610601+primals@users.noreply.github.com>
2021-03-19 13:17:49 -0600

    added more base converter tests

--- app/app/src/test/java/com/DivineInspiration/experimenter/TestIdGen.java
+++ app/app/src/test/java/com/DivineInspiration/experimenter/TestIdGen.java
@@ -6,10 +6,24 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 public class TestIdGen {
     @Test
-    public void baseConversion(){
+    public void baseConversionTest(){
         long l = 1679616L;
-        assertEquals("10", IdGen.base10To36(l));
+        assertEquals("10000", IdGen.base10To36(l));
         assertEquals(1679616L, IdGen.base36To10(IdGen.base10To36(l)));
+        long x = 123456L;
+        assertEquals("2N9C", IdGen.base10To36(x));
+        assertEquals(123456, IdGen.base36To10("2N9C"));
+        assertNotEquals(123, IdGen.base36To10("2N9C"));
+        assertNotEquals("ABCD", IdGen.base10To36(x));
+
+        long f = 0L;
+        assertEquals("0", IdGen.base10To36(f));
+        assertEquals(0, IdGen.base36To10("0"));
+
+        long y = -152L;
+        assertEquals("-48", IdGen.base10To36(y));
+        assertEquals(-152, IdGen.base36To10("-48"));
+
     }
     @Test
     public void testId(){
