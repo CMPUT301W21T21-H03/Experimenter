c662186574e06a0ab75d6cb60448bde67e22a0de
Aniket Mishra <ianiket23@gmail.com>
2021-03-25 11:35:27 -0600

    QR code update
    fixed few things

--- app/app/src/main/java/com/DivineInspiration/experimenter/Activity/UI/Scan/ScanFragment.java
+++ app/app/src/main/java/com/DivineInspiration/experimenter/Activity/UI/Scan/ScanFragment.java
@@ -1,41 +1,42 @@
 package com.DivineInspiration.experimenter.Activity.UI.Scan;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.EditText;
+
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.DivineInspiration.experimenter.Controller.ExperimentManager;
+import com.DivineInspiration.experimenter.Model.Experiment;
 import com.DivineInspiration.experimenter.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ScanFragment extends Fragment {
 
-    private DashboardViewModel dashboardViewModel;
 
-    /**
-     * On create fragment
-     * @param inflater
-     * @param container
-     * @param savedInstanceState
-     * @return
-     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // inits
-        dashboardViewModel = new ViewModelProvider(this).get(DashboardViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_scan, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
 
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            // when text view changes, update text view
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        // query from database
+        View root = inflater.inflate(R.layout.fragment_scan, container, false);
         return root;
     }
 }
