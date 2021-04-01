c9f85c97d34ac6dc536c3c2cf78f527a3e8188fc
Aniket Mishra <ianiket23@gmail.com>
2021-04-01 11:18:05 -0600

    demo qrcode scanner

--- app/app/src/main/java/com/DivineInspiration/experimenter/Activity/UI/Scan/ScanFragment.java
+++ app/app/src/main/java/com/DivineInspiration/experimenter/Activity/UI/Scan/ScanFragment.java
@@ -1,5 +1,6 @@
 package com.DivineInspiration.experimenter.Activity.UI.Scan;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -8,8 +9,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -18,12 +21,14 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.DivineInspiration.experimenter.Controller.ExperimentManager;
 import com.DivineInspiration.experimenter.Model.Experiment;
 import com.DivineInspiration.experimenter.R;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ScanFragment extends Fragment {
-
+    View scannerView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,5 +43,26 @@ public class ScanFragment extends Fragment {
         // query from database
         View root = inflater.inflate(R.layout.fragment_scan, container, false);
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        scannerView = view.findViewById(R.id.scannerView);
+        new IntentIntegrator(this.getActivity()).initiateScan(); // `this` is the current Activity
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this.getActivity(), "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this.getActivity(), "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
