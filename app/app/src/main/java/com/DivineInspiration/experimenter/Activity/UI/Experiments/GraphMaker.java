59dee92a4fc53bf775cafd62f47d391bbf1cf8e6
Goldentoaste <rayg2375@gmail.com>
2021-04-02 22:31:38 -0700

    sutff

--- app/app/src/main/java/com/DivineInspiration/experimenter/Activity/UI/Experiments/GraphMaker.java
+++ app/app/src/main/java/com/DivineInspiration/experimenter/Activity/UI/Experiments/GraphMaker.java
@@ -1,8 +1,28 @@
 package com.DivineInspiration.experimenter.Activity.UI.Experiments;
 
+import com.DivineInspiration.experimenter.Model.Trial.Trial;
+import com.github.mikephil.charting.charts.Chart;
+import com.github.mikephil.charting.data.CandleData;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class GraphMaker {
 
 
+    //!! this assumes all trials are of the same kind
+    public Chart makeHistogram(ArrayList<Trial> trials){
 
+
+        return null;
+    }
+
+
+    public Chart makeLineChart(ArrayList<Trial> trials){
+
+        return null;
+    }
+
+    
 
 }
