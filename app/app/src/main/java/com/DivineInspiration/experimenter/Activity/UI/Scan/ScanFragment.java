7553a450caf112e05cab699c110d1cd52d90054a
Zeyu-Li <zeyu.li23@gmail.com>
2021-04-05 21:00:39 -0600

    finished qr scanner with new library (works with barcodes)

--- app/app/src/main/java/com/DivineInspiration/experimenter/Activity/UI/Scan/ScanFragment.java
+++ app/app/src/main/java/com/DivineInspiration/experimenter/Activity/UI/Scan/ScanFragment.java
@@ -1,5 +1,6 @@
 package com.DivineInspiration.experimenter.Activity.UI.Scan;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
@@ -8,6 +9,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -18,9 +20,14 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.DivineInspiration.experimenter.Activity.MainActivity;
 import com.DivineInspiration.experimenter.Controller.ExperimentManager;
 import com.DivineInspiration.experimenter.Model.Experiment;
 import com.DivineInspiration.experimenter.R;
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
@@ -28,28 +35,54 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ScanFragment extends Fragment {
+    private CodeScanner mCodeScanner;
     View scannerView;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
-
+    Button scan;
+    // scanned code
+    String scanned;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-        // query from database
+        // from https://www.youtube.com/watch?v=drH63NpSWyk & https://github.com/yuriy-budiyev/code-scanner
+        final Activity activity = getActivity();
         View root = inflater.inflate(R.layout.fragment_scan, container, false);
+        CodeScannerView scannerView = root.findViewById(R.id.scanner);
+        mCodeScanner = new CodeScanner(activity, scannerView);
+        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        scanned = result.getText();
+
+                        Toast.makeText(activity, "Code scanned successfully", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+        scannerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCodeScanner.startPreview();
+            }
+        });
         return root;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        scannerView = view.findViewById(R.id.scannerView);
-        new IntentIntegrator(this.getActivity()).initiateScan(); // `this` is the current Activity
+//        scannerView = view.findViewById(R.id.scannerView);
+        scan = view.findViewById(R.id.scanButton);
+//        new IntentIntegrator(this.getActivity()).initiateScan(); // `this` is the current Activity
+//
+//        scan.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                new IntentIntegrator(view.getActivity()).initiateScan(); // `this` is the current Activity
+//            }
+//        });
     }
 
     @Override
@@ -64,5 +97,21 @@ public class ScanFragment extends Fragment {
         } else {
             super.onActivityResult(requestCode, resultCode, data);
         }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mCodeScanner.startPreview();
+    }
+
+    @Override
+    public void onPause() {
+        mCodeScanner.releaseResources();
+        super.onPause();
+    }
+
+    private void setupPermissions() {
+        int permission =
     }
 }
