b76ccf6c74f96569d9db41de4dfaf04ec859a9c3
Adit Rada <rada_adit@hotmail.com>
2021-04-07 17:57:45 +0300

    Added documentation for the Trials (Model)

--- app/app/src/main/java/com/DivineInspiration/experimenter/Model/Trial/CountTrial.java
+++ app/app/src/main/java/com/DivineInspiration/experimenter/Model/Trial/CountTrial.java
@@ -14,9 +14,10 @@ import java.util.UUID;
 public class CountTrial extends Trial {
     private int count;
 
-
-
-    //constructor with location
+    /**
+     * Constructor
+     * @param: trialId:String, userId:String, trialOwnerName:String, trialExperimentID:String, date:LocalDate, count:int, location:GeoPoint
+     */
     public CountTrial(String trialID, String trialUserID,String trialOwnerName ,String trialExperimentID, LocalDate trialDate, int count, GeoPoint location){
         super(trialID, trialUserID,trialOwnerName ,trialExperimentID, trialDate, location);
         this.trialType = Trial.COUNT;
@@ -25,22 +26,17 @@ public class CountTrial extends Trial {
 
     /**
      * Constructor
-     * @param trialUserID
-     * user of this trial
-     * @param trialExperimentID
-     * id of experiment
+     * @param: trialId:String, userId:String, trialOwnerName:String, trialExperimentID:String, date:LocalDate
      */
     public CountTrial(String trialUserID,String trialOwnerName ,String trialExperimentID) {
         super(trialUserID,trialOwnerName ,trialExperimentID);
         this.trialType = Trial.COUNT;
         this.count = 0;
     }
+
     /**
      * Constructor
-     * @param trialUserID
-     * user of this trial
-     * @param trialExperimentID
-     * id of experiment
+     * @param: trialId:String, userId:String, trialOwnerName:String, trialExperimentID:String, date:LocalDate, count:int, location:GeoPoint
      */
     public CountTrial(String trialUserID,String trialOwnerName ,String trialExperimentID, int count, GeoPoint location) {
         super(trialUserID,trialOwnerName ,trialExperimentID);
@@ -49,30 +45,24 @@ public class CountTrial extends Trial {
         this.location = location;
     }
 
-
-    public CountTrial(){
-        super("test", "test", "test","test" ,LocalDate.now().plusDays(new Random().nextInt(20)));
-        this.trialType = Trial.COUNT;
-        this.count =new Random().nextInt(20);
-    }
-
     /**
      * Constructor
-     * @param trialID
-     * the id of this trial
-     * @param trialDate
-     * the date of this trial
-     * @param trialUserID
-     * user of this trial
-     * @param trialExperimentID
-     * id of experiment
-     * @param count
-     * count value for this trial
+     * @param: trialId:String, userId:String, trialOwnerName:String, trialExperimentID:String, date:LocalDate, count:int
      */
     public CountTrial(String trialID, String trialUserID,String trialOwnerName ,String trialExperimentID, LocalDate trialDate, int count) {
         super(trialID, trialUserID, trialOwnerName ,trialExperimentID, trialDate);
         this.trialType = Trial.COUNT;
         this.count = count;
+    }
+
+    /**
+     * Mock object constructor for testing purposes
+     * @param: void
+     */
+    public CountTrial(){
+        super("test", "test", "test","test" ,LocalDate.now().plusDays(new Random().nextInt(20)));
+        this.trialType = Trial.COUNT;
+        this.count =new Random().nextInt(20);
     }
 
     /**
@@ -83,9 +73,8 @@ public class CountTrial extends Trial {
     }
 
     /**
-     * Sets the count
-     * @param count
-     * new count
+     * Sets count
+     * @param: count:int (new count)
      */
     public void setCount(int count) {
         this.count = count;
@@ -100,7 +89,7 @@ public class CountTrial extends Trial {
 
     /**
      * Gets current count
-     * @return: count
+     * @return: count:int
      */
     public int getCount() {
         return count;
