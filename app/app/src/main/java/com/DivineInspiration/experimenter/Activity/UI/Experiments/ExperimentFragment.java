b82e1a148f928bd1ffc5e06e61f3b49d1f4eec4e
jamessch16 <jamessch16@gmail.com>
2021-04-09 03:54:44 -0600

    merge fix

--- app/app/src/main/java/com/DivineInspiration/experimenter/Activity/UI/Experiments/ExperimentFragment.java
+++ app/app/src/main/java/com/DivineInspiration/experimenter/Activity/UI/Experiments/ExperimentFragment.java
@@ -426,33 +426,4 @@ public class ExperimentFragment extends Fragment implements Subject, TrialManage
             return 4;
         }
     }
-
-
-    /**
-     * Placeholder fragment
-     */
-    public static class PlaceHolderFragment extends Fragment {
-        @Nullable
-        @Override
-        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                                 @Nullable Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.test, container, false);
-        }
-
-        @Override
-        public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-            super.onViewCreated(view, savedInstanceState);
-            ListView list = view.findViewById(R.id.placeHolderList);
-
-            String[] items = {"Russell’s", "Paradox", "tells", "us", "that", "Humans", "are", "bad", "at", "math.",
-                    "Our", "intuitions", "lead", "us", "astray.", "Things", "that", "look", "reasonable,", "can", "be",
-                    "completely", "wrong.", "So", "we", "have", "to", "be", "very", "very", "careful,", "very", "very",
-                    "precise,", "very", "very", "logical.", "We", "don’t", "want", "to", "be,", "but", "we", "have",
-                    "to", "be.", "Or", "we’ll", "get", "into", "all", "kinds", "of", "trouble.", "So", "let’s",
-                    "describe", "the", "grammar", "of", "math,", "which", "is", "logic!"};
-            ArrayAdapter<String> adapter = new ArrayAdapter<>(view.getContext(), R.layout.test_item, items);
-
-            list.setAdapter(adapter);
-        }
-    }
 }
