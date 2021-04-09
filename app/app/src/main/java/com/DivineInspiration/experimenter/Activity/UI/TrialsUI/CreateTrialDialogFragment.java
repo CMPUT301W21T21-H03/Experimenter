31935f17015cf0db718b65c5f17382b5267fdf68
Aniket Mishra <ianiket23@gmail.com>
2021-04-09 06:17:05 -0600

    Update CreateTrialDialogFragment.java

--- app/app/src/main/java/com/DivineInspiration/experimenter/Activity/UI/TrialsUI/CreateTrialDialogFragment.java
+++ app/app/src/main/java/com/DivineInspiration/experimenter/Activity/UI/TrialsUI/CreateTrialDialogFragment.java
@@ -160,7 +160,7 @@ public class CreateTrialDialogFragment extends DialogFragment implements EasyPer
                                 measure = measurementTextBox.getText().toString();
                             }else{
                                 measure = args.getString("Value");
-                                countNNTrial.setText(measure);
+                                measurementTextBox.setText(measure);
 
                             }
                             measurementTrialDialog(args, exp, measure);
@@ -449,14 +449,8 @@ public class CreateTrialDialogFragment extends DialogFragment implements EasyPer
                 break;
 
             case "Measurement trial":
-                if(args.getBoolean("isScan") == false){
-                    measurementTextBox.setVisibility(View.VISIBLE);
-                    valueHolder.setVisibility(View.VISIBLE);
-                }else{
-                    measurementTextBox.setVisibility(View.GONE);
-                    valueHolder.setVisibility(View.GONE);
-                    countNNTrial.setVisibility(View.VISIBLE);
-                }
+                measurementTextBox.setVisibility(View.VISIBLE);
+                valueHolder.setVisibility(View.VISIBLE);
 
                 break;
 
