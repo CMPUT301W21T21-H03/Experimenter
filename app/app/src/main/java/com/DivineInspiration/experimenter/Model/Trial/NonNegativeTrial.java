9753c6ada851273e59725fff2a2473006d3e3640
jamessch16 <jamessch16@gmail.com>
2021-04-09 11:28:07 -0600

    remove unused methods

--- app/app/src/main/java/com/DivineInspiration/experimenter/Model/Trial/NonNegativeTrial.java
+++ app/app/src/main/java/com/DivineInspiration/experimenter/Model/Trial/NonNegativeTrial.java
@@ -1,14 +1,8 @@
 package com.DivineInspiration.experimenter.Model.Trial;
 
-import com.DivineInspiration.experimenter.Model.User;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.firebase.firestore.GeoPoint;
-
 
 import java.time.LocalDate;
-import java.util.Date;
-import java.util.Random;
-import java.util.UUID;
 
 public class NonNegativeTrial extends Trial {
     private int count;
@@ -30,27 +24,11 @@ public class NonNegativeTrial extends Trial {
      * @param location
      * location of where the trial occurred
      */
-    public NonNegativeTrial(String trialID, String trialUserID,String trialOwnerName ,String trialExperimentID, LocalDate trialDate, int count, LatLng location){
-        super(trialID, trialUserID, trialOwnerName ,trialExperimentID, trialDate,location);
+    public NonNegativeTrial(String trialID, String trialUserID, String trialOwnerName, String trialExperimentID, LocalDate trialDate, int count, LatLng location){
+        super(trialID, trialUserID, trialOwnerName, trialExperimentID, trialDate,location);
         this.trialType = Trial.NONNEGATIVE;
         this.count = count;
     }
-
-    /**
-     * The constructor
-     * @param trialUserID
-     * ID of user
-     * @param trialOwnerName
-     * name of the experimenter that did the trial
-     * @param trialExperimentID
-     * ID of the experiment
-     */
-    public NonNegativeTrial(String trialUserID, String trialOwnerName, String trialExperimentID) {
-        super(trialUserID, trialOwnerName,trialExperimentID);
-        this.trialType = Trial.NONNEGATIVE;
-        this.count = 0;
-    }
-
 
     /**
      * The constructor
@@ -65,13 +43,12 @@ public class NonNegativeTrial extends Trial {
      * @param location
      * location of where the trial occurred
      */
-    public NonNegativeTrial(String trialUserID,String trialOwnerName ,String trialExperimentID, int count, LatLng location) {
-        super(trialUserID, trialOwnerName,trialExperimentID);
+    public NonNegativeTrial(String trialUserID, String trialOwnerName, String trialExperimentID, int count, LatLng location) {
+        super(trialUserID, trialOwnerName, trialExperimentID);
         this.trialType = Trial.NONNEGATIVE;
         this.count = count;
         this.location = location;
     }
-
 
     /**
      * The constructor
@@ -88,27 +65,10 @@ public class NonNegativeTrial extends Trial {
      * @param count
      * number of count
      */
-    public NonNegativeTrial(String trialID, String trialUserID,String trialOwnerName ,String trialExperimentID, LocalDate trialDate, int count) {
+    public NonNegativeTrial(String trialID, String trialUserID, String trialOwnerName, String trialExperimentID, LocalDate trialDate, int count) {
         super(trialID, trialUserID,trialOwnerName ,trialExperimentID, trialDate);
         this.trialType = Trial.NONNEGATIVE;
         this.count = count;
-    }
-
-    /**
-     * Mock object constructor for testing purposes
-     */
-    public NonNegativeTrial() {
-        super("test", "test", "test","test", LocalDate.now().plusDays(new Random().nextInt(40) - 20));
-        this.trialType = Trial.NONNEGATIVE;
-        Random rng = new Random();
-        count = rng.nextInt(20);
-    }
-
-    /**
-     * Increments count by one
-     */
-    public void addCount() {
-        ++count;
     }
 
     /**
